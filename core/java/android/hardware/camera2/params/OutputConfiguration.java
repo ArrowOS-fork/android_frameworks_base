c883419ce6136d2202a56c461bfc60feb1eadca5
Cole Faust <colefaust@google.com>
2022-10-15 21:33:29 -0700

    Fix errorprone warnings that should be errors
    This commit is part of a large scale change to fix errorprone
    errors that have been downgraded to warnings in the android
    source tree, so that they can be promoted to errors again.
    The full list of changes include the following, but not all
    will be present in any one individual commit:
    
    BadAnnotationImplementation
    BadShiftAmount
    BanJNDI
    BoxedPrimitiveEquality
    ComparableType
    ComplexBooleanConstant
    CollectionToArraySafeParameter
    ConditionalExpressionNumericPromotion
    DangerousLiteralNull
    DoubleBraceInitialization
    DurationFrom
    DurationTemporalUnit
    EmptyTopLevelDeclaration
    EqualsNull
    EqualsReference
    FormatString
    FromTemporalAccessor
    GetClassOnAnnotation
    GetClassOnClass
    HashtableContains
    IdentityBinaryExpression
    IdentityHashMapBoxing
    InstantTemporalUnit
    InvalidTimeZoneID
    InvalidZoneId
    IsInstanceIncompatibleType
    JUnitParameterMethodNotFound
    LockOnBoxedPrimitive
    MathRoundIntLong
    MislabeledAndroidString
    MisusedDayOfYear
    MissingSuperCall
    MisusedWeekYear
    ModifyingCollectionWithItself
    NoCanIgnoreReturnValueOnClasses
    NonRuntimeAnnotation
    NullableOnContainingClass
    NullTernary
    OverridesJavaxInjectableMethod
    ParcelableCreator
    PeriodFrom
    PreconditionsInvalidPlaceholder
    ProtoBuilderReturnValueIgnored
    ProtoFieldNullComparison
    RandomModInteger
    RectIntersectReturnValueIgnored
    ReturnValueIgnored
    SelfAssignment
    SelfComparison
    SelfEquals
    SizeGreaterThanOrEqualsZero
    StringBuilderInitWithChar
    TreeToString
    TryFailThrowable
    UnnecessaryCheckNotNull
    UnusedCollectionModifiedInPlace
    XorPower
    
    See https://errorprone.info/bugpatterns for more
    information on the checks.
    
    Bug: 253827323
    Test: m RUN_ERROR_PRONE=true javac-check
    Change-Id: I8446f9076a45ebf7e7ffa06cb0d4ddb1001b6c00
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- core/java/android/hardware/camera2/params/OutputConfiguration.java
+++ core/java/android/hardware/camera2/params/OutputConfiguration.java
@@ -1291,7 +1291,8 @@ public final class OutputConfiguration implements Parcelable {
                 return false;
             }
             for (int j = 0; j < mSensorPixelModesUsed.size(); j++) {
-                if (mSensorPixelModesUsed.get(j) != other.mSensorPixelModesUsed.get(j)) {
+                if (!Objects.equals(
+                        mSensorPixelModesUsed.get(j), other.mSensorPixelModesUsed.get(j))) {
                     return false;
                 }
             }
